e4bfc5cdca2fdc46a7ece8918d23d11d96b8a170
Julia Hohulin <97487349+hohojl@users.noreply.github.com>
2022-05-23 14:59:42 -0500

    Add files via upload

---
+++ form.js
@@ -0,0 +1,29 @@
+// user input - includes validation
+function TodoForm({addTodo}){
+  const [value,setValue] = React.useState('');
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add Todo..."
+        onChange={e => setValue(e.target.value)} />
+    </form>
+  )
+}
+
+const removeTodo = e => {
+  const index = Number(e.target.id);
+  let temp = [...todos];
+  temp.splice(index,1);
+  setTodos(temp);
+}
